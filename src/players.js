0c1d71c0fea5c320bc8f6c6a674272a7019ca71b
mbouchriha <mbouchriha31@gmail.com>
2023-08-30 12:59:23 +0100

    player card

---
+++ src/players.js
@@ -0,0 +1,22 @@
+// src/players.js
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Paris Saint-Germain",
+    nationality: "Argentina",
+    jerseyNumber: 30,
+    age: 34,
+    imageUrl: "https://tse1.explicit.bing.net/th?id=OIP.jKHBRVWDytTl9XLqRRQ7kAHaJ4&pid=Api&P=0&h=180",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Manchester United",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 36,
+    imageUrl: "https://tse4.mm.bing.net/th?id=OIF.%2baDb8w4Tk5Ihi0Ch8y8%2b8w&pid=Api&P=0&h=180",
+  },
+  // Add more players here
+];
+
+export default players;
